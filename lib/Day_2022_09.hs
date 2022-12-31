f092d81e55750aa05f06ebb26339af2f8381ae43
Mathijs Kwik <mathijs@bluescreen303.nl>
2022-12-31 09:54:33 +0100

    add 2022-09

---
+++ lib/Day_2022_09.hs
@@ -0,0 +1,36 @@
+module Day_2022_09 where
+
+import Data.List (scanl', nub)
+
+-- parse the puzzle
+type Move = (Int, Int) -> (Int, Int)
+
+toMove :: [String] -> [Move]
+toMove [d, c] = replicate a (m d)
+    where m "U" (x, y) = (x, y + 1)
+          m "D" (x, y) = (x, y - 1)
+          m "L" (x, y) = (x - 1, y)
+          m "R" (x, y) = (x + 1, y)
+          m e   _      = error $ "unknown move: " ++ e
+          a = read c
+toMove e      = error $ "unknown move: " ++ unwords e
+
+parse :: String -> [Move]
+parse = concatMap (toMove . words) . lines
+
+-- run the rope
+
+headLocations :: [Move] -> [(Int, Int)]
+headLocations = scanl' (flip id) (0, 0)
+
+tailLocations :: [(Int, Int)] -> [(Int, Int)]
+tailLocations = tail . scanl' moveTail (0, 0)
+    where moveTail t@(tailX, tailY) (headX, headY)
+              | distance <= 1 = t
+              | otherwise     = (tailX + signum diffX, tailY + signum diffY)
+            where diffX    = headX - tailX
+                  diffY    = headY - tailY
+                  distance = max (abs diffX) (abs diffY)
+
+main :: String -> Int
+main = length . nub . tailLocations . headLocations . parse
