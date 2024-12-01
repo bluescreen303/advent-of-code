00bfa976649633e02c850fc96924e19c3a5de155
Mathijs Kwik <mathijs.kwik@ing.com>
2024-12-01 23:02:26 +0100

    restructure to allow jq and rust solutions

--- flake.nix
+++ flake.nix
@@ -11,13 +11,41 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, bluenix }:
+    with nixpkgs.lib;
     flake-utils.lib.eachSystem ["x86_64-linux"] (system:
       let pkgs = bluenix.legacyPackages.${system};
           hp = pkgs.haskellPackages;
       in
       {
-        packages.default = hp.callPackage ./advent-of-code.nix {};
-        devShells.default = bluenix.lib.haskell-shell hp self.packages.${system}.default;
+        packages = {
+          aoc-hs = hp.callPackage aoc-hs/advent-of-code.nix {};
+          aoc-rs = let src = ./aoc-rs;
+                     cargoTOML = (fromTOML (readFile (src + /Cargo.toml)));
+                     pname     = cargoTOML.package.name;
+                     version   = cargoTOML.package.version;
+                   in
+                     pkgs.callPackage ({ inShell ? false }: pkgs.rustPlatform.buildRustPackage {
+                       inherit pname version src;
+
+                       cargoLock.lockFile = src + /Cargo.lock;
+
+                       nativeBuildInputs = optionals inShell [
+                         pkgs.rustfmt
+                         pkgs.clippy
+                         pkgs.rust-analyzer
+                       ];
+                     }) {};
+          default = self.packages.${system}.aoc-rs;
+        };
+        devShells = {
+          aoc-hs = bluenix.lib.haskell-shell hp self.packages.${system}.aoc-hs;
+          aoc-rs = self.packages.${system}.aoc-rs.override { inShell = true; };
+          aoc-jq = pkgs.mkShell {
+            name = "aoc-jq-shell";
+            packages = [ pkgs.jaq ];
+          };
+          default = self.devShells.${system}.aoc-rs;
+        };
       }
     );
 }
